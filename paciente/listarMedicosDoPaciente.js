19f160b79ec4b2d15915cac2dcf7e76198fab37a
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-09-09 19:34:01 -0300

    f

--- paciente/listarMedicosDoPaciente.js
+++ paciente/listarMedicosDoPaciente.js
@@ -3,13 +3,13 @@ let {consultas, medicos} = require('../data')
 function listarMedicosDoPaciente(req, res){
     let {idPaciente}= req.query
 
-    const Paciente = consultas.filter(p => p.idPaciente == idPaciente)
+    const consultaPaciente = consultas.filter(p => p.idPaciente == idPaciente)
 
     if(Paciente.length === 0){
         return res.status(404).send('medicos do paciente não encontrado!')
     }
 
-    const idMedico= Paciente.map(p => p.idMedico)
+    const idMedico= consultaPaciente.map(p => p.idMedico)
     const medico= medicos.filter(m => idMedico.includes(m.id))
     const medicoDoPaciente= medico.map(m => ({nome: m.nome, especialidade: m.especialidade}))
 
